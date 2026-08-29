df5172ef9f6f26e35e97dde17b96845acadbca7f
Abhishek Jadon <abhisheksode001@gmail.com>
2026-08-29 17:34:37 +0530

    optimize: enable ISR static caching across all website routes

--- src/components/providers/AppProviders.jsx
+++ src/components/providers/AppProviders.jsx
@@ -1,16 +1,14 @@
 "use client";
 
 import React from "react";
-import dynamic from "next/dynamic";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { App, ConfigProvider } from "antd";
 
 import SWRProvider from "@/components/providers/SWRProvider";
+import CompareDrawerWidget from "@/components/website/CompareDrawerWidget";
 import JsonLd from "@/components/common/JsonLd";
 import { ToolWizardProvider } from "@/components/tool/ToolWizardContext";
-
-const CompareDrawerWidget = dynamic(() => import("@/components/website/CompareDrawerWidget"), { ssr: false });
-const AutoEngineToolModal = dynamic(() => import("@/components/tool/AutoEngineToolModal"), { ssr: false });
+import AutoEngineToolModal from "@/components/tool/AutoEngineToolModal";
 
 /**
  * AppProviders - Consolidated System Wrapper for SODE Application
