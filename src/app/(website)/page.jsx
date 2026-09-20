b41a8704039de873d2d45feaf1c9670e75e340a8
Abhishek Jadon <abhisheksode001@gmail.com>
2026-09-21 01:43:21 +0530

    adf

--- src/app/(website)/page.jsx
+++ src/app/(website)/page.jsx
@@ -42,7 +42,7 @@ export default async function Home() {
     coursesData,
     categoryApiData,
   ] = await Promise.all([
-    request.dynamicRead({ entity: "hero", endPoint: "public/by-slug", slug: "home", revalidate: 30 }),
+    request.dynamicRead({ entity: "hero", endPoint: "public/by-slug", slug: "home", revalidate: 300 }),
     getUniversitiesData(),
     request.dynamicList({ entity: "courses", endPoint: "v1/list", options: { items: 30 }, revalidate: 300 }),
     request.dynamicList({ entity: "category", endPoint: "v1/list", revalidate: 900 }),
