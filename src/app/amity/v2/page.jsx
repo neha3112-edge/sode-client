bdf6840976f968f6801b36d64b46f02ff6122147
Abhishek Jadon <abhisheksode001@gmail.com>
2026-09-22 22:46:51 +0530

    refactor: remove /amity/v2 page, keep only standard /amity

--- src/app/amity/v2/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UniversityLandingView from "@/components/landing/UniversityLandingView";
-import { amityData } from "@/data/landing/amity";
-
-export const metadata = amityData.meta;
-
-export default function AmityV2Page() {
-  return <UniversityLandingView data={amityData} />;
-}
