a12bc777edf132469058b76d0ee59d32f5f4e35f
Abhishek Jadon <abhisheksode001@gmail.com>
2026-09-22 22:45:52 +0530

    refactor: remove deprecated hardcoded src/components/Amity folder in favor of modular src/components/landing

--- src/components/Amity/AmityLandingView.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-import UniversityLandingView from "@/components/landing/UniversityLandingView";
-import { amityData } from "@/data/landing/amity";
-
-export default function AmityLandingView() {
-  return <UniversityLandingView data={amityData} />;
-}
